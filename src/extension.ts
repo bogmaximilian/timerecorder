cb2c3ac68534f247e7a48e12caf8cf7425b4753f
bogmaximilian <49530504+bog.maximilian@users.noreply.github.com>
2021-04-01 20:24:12 +0200

    Fixed everything i think idk idc anymore

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { InputBoxOptions } from 'vscode';
 import * as fs from 'fs';
 
 let recordstart: string;
 let isRecording = false;
 
 
-function writetoLog(){
+function writetoLog() {
 	//Set recording end
 	const recordend = new Date();
 
@@ -16,11 +15,28 @@ function writetoLog(){
 	const path = vscode.workspace.getConfiguration('timerecorder').get('defaultlogpath');
 
 	//Write to file
-	fs.appendFile(path + `${recordend.getDate()}`, `${recordstart} ${recordend.toLocaleTimeString()}`, (err)  => {
+	var regex = /\//gi; 
+	fs.appendFile(path + `\\${recordend.toLocaleDateString().replace(regex, '_')}.txt`, `[${recordstart}-${recordend.toLocaleTimeString()}]\n`, (err)  => {
 		if(err) {
 			console.log(err);
 		}
 	});
+}
+
+function setdefaultlogpath() {
+	//Open Select Folder Dialog
+	const options: vscode.OpenDialogOptions = {
+		canSelectMany: false,
+		openLabel: 'Select',
+		canSelectFiles: false,
+		canSelectFolders: true
+	};
+   
+   vscode.window.showOpenDialog(options).then(fileUri => {
+	   if (fileUri && fileUri[0]) {
+		   vscode.workspace.getConfiguration('timerecorder').update('defaultlogpath', fileUri[0].fsPath, true);
+	   }
+   });
 }
 
 // this method is called when your extension is activated
@@ -29,27 +45,19 @@ export function activate(context: vscode.ExtensionContext) {
 	//Check if Log Path has already been set
 	const logpath = vscode.workspace.getConfiguration('timerecorder').defaultlogpath;
 	if(logpath === '') {
-		//Open Select Folder Dialog
-		const options: vscode.OpenDialogOptions = {
-			canSelectMany: false,
-			openLabel: 'Select',
-			canSelectFiles: false,
-			canSelectFolders: true
-		};
-	   
-	   vscode.window.showOpenDialog(options).then(fileUri => {
-		   if (fileUri && fileUri[0]) {
-			   vscode.workspace.getConfiguration('timerecorder').update('defaultlogpath', fileUri[0].fsPath, true);
-		   }
-	   });
+		setdefaultlogpath();
 	}
+
+	let logpathcmd = vscode.commands.registerCommand('timerecorder.setdefaultlogpath', () => {
+		setdefaultlogpath();
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let startcmd = vscode.commands.registerCommand('timerecorder.startrecording', () => {
 		if(isRecording) {
-			let items: string[] = ["Stop current recording, log it to file and start new recording", "Stop current recording"]; 
+			let items: string[] = ["Stop current recording, log it to file and start new recording", "Stop current recording log it to file"]; 
 			
 			vscode.window.showQuickPick(items).then(value => {
 				isRecording = true;
@@ -58,7 +66,7 @@ export function activate(context: vscode.ExtensionContext) {
 					//Stop current recording log and start new options
 					case 0:
 						writetoLog();
-
+						
 						recordstart = new Date().toLocaleTimeString();
 						isRecording = true;
 						break;
@@ -66,19 +74,25 @@ export function activate(context: vscode.ExtensionContext) {
 					//Just stop recording
 					case 1:
 						writetoLog();
+						isRecording = false;
 						break;
 				}
 			});
 		}
+		else {
+			//Start recording
+			recordstart = new Date().toLocaleTimeString();
+			isRecording = true;
+			//Log output to user
+			vscode.window.showInformationMessage('Started Recording at ' + recordstart);
+		}
 		
-		//Log output to user
-		vscode.window.showInformationMessage('Started Recording at ' + recordstart);
 	});
 
 	let stopcmd = vscode.commands.registerCommand('timerecorder.stoprecording', () => {
-		isRecording = true;
 
-		let items: string[] = ["Stop current recording, log it to file", "Stop current recording and start new one"]; 
+		if(isRecording) {
+			let items: string[] = ["Stop current recording, log it to file", "Stop current recording and start new one"]; 
 			
 			vscode.window.showQuickPick(items).then(value => {
 				isRecording = true;
@@ -99,8 +113,9 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			});
 
-		//Log output to user
-		vscode.window.showInformationMessage('Started Recording at ');
+			//Log output to user
+			vscode.window.showInformationMessage('Stopped Recording at ' + new Date().toLocaleTimeString());
+		}
 	});
 
 	context.subscriptions.push(startcmd);
